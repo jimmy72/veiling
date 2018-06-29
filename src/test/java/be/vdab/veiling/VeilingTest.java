88a1c9b3ddf69c14f2a40e577c7d5f9919f6a9bb
Jimmy <Jimmy@PC-JIMMY.home>
2018-06-29 14:07:11 +0200

    @before

--- src/test/java/be/vdab/veiling/VeilingTest.java
+++ src/test/java/be/vdab/veiling/VeilingTest.java
@@ -2,25 +2,30 @@ package be.vdab.veiling;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 public class VeilingTest {
 
+	private Veiling veiling;
+	
+	@Before
+	public void before() {
+		veiling = new Veiling();
+	}
+	
 	@Test
 	public void als_nog_geen_bod_is_gedaan_dan_is_het_hoogste_bod_nul() {
-		Veiling veiling = new Veiling();
 		assertEquals(0, veiling.getHoogsteBod());
 	}
 	
 	@Test
 	public void als_er_een_eerste_bod_werd_uitgevoerd_dan_is_hoogste_bod_eerste_bod() {
-		Veiling veiling = new Veiling();
 		veiling.doeBod(100);
 		assertEquals(100, veiling.getHoogsteBod());
 	}
 	
 	@Test
 	public void als_er_meermaals_bod_werd_uitgevoerd_dan_is_hoogste_bod_gelijk_aan_hoogste_bod() {
-		Veiling veiling = new Veiling();
 		veiling.doeBod(100);
 		veiling.doeBod(50);
 		veiling.doeBod(150);
